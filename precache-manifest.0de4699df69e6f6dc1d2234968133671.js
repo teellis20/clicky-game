202e4ec915888ec657fae57b28ba6ae886143d59
Tellis <teellis20@gmail.com>
2019-12-16 18:55:27 -0800

    Updates

---
+++ precache-manifest.0de4699df69e6f6dc1d2234968133671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f755f42b8acfc585cdd05e6bcc7b5f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1839671d95ac157db74e",
+    "url": "/clicky-game/static/css/main.d04a4a10.chunk.css"
+  },
+  {
+    "revision": "080e557a7b5ee1020a25",
+    "url": "/clicky-game/static/js/2.a7c8e9ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.a7c8e9ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1839671d95ac157db74e",
+    "url": "/clicky-game/static/js/main.247da6a2.chunk.js"
+  },
+  {
+    "revision": "3a7399c0e45a8c84508d",
+    "url": "/clicky-game/static/js/runtime-main.7bc1c67f.js"
+  }
+]);
